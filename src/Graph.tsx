226d8f48439b2b376ca3d056d7a06f4e1a3e58bb
Prdp41 <119116530+Prdp41@users.noreply.github.com>
2024-07-31 14:08:27 +0530

    Initial Commit

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
 import { Table } from '@finos/perspective';
+import React, { Component } from 'react';
 import { ServerRespond } from './DataStreamer';
 import './Graph.css';
 
@@ -14,7 +14,7 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
+interface PerspectiveViewerElement extends HTMLElement{
   load: (table: Table) => void,
 }
 
@@ -32,8 +32,7 @@ class Graph extends Component<IProps, {}> {
 
   componentDidMount() {
     // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
-
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
     const schema = {
       stock: 'string',
       top_ask_price: 'float',
@@ -41,15 +40,22 @@ class Graph extends Component<IProps, {}> {
       timestamp: 'date',
     };
 
-    if (window.perspective && window.perspective.worker()) {
+    if (window.perspective ) {
       this.table = window.perspective.worker().table(schema);
     }
     if (this.table) {
-      // Load the `table` in the `<perspective-viewer>` DOM reference.
+      // Load the table in the <perspective-viewer> DOM reference.
 
       // Add more Perspective configurations here.
+
       elem.load(this.table);
+      elem.setAttribute('view', 'y_line');
+      elem.setAttribute('column-pivots', '["stock"]');
+      elem.setAttribute('row-pivots', '["timestamp"]');
+      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('aggregates','{"stock": "distinct count","top_ask_price":"avg","top_bid_price":"avg","timestamp":"distinct count"}');
     }
+    
   }
 
   componentDidUpdate() {
@@ -61,8 +67,8 @@ class Graph extends Component<IProps, {}> {
         // Format the data from ServerRespond to the schema
         return {
           stock: el.stock,
-          top_ask_price: el.top_ask && el.top_ask.price || 0,
-          top_bid_price: el.top_bid && el.top_bid.price || 0,
+          top_ask_price: (el.top_ask && el.top_ask.price) || 0,
+          top_bid_price: (el.top_bid && el.top_bid.price) || 0,
           timestamp: el.timestamp,
         };
       }));
